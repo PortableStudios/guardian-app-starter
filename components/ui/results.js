0307e5d25882f606db1d96936589d65240598115
mattvioli <mattvioli@gmail.com>
2021-08-11 14:45:41 +1000

    loops through  to get sections, need to filter out repeats

--- components/ui/results.js
+++ components/ui/results.js
@@ -2,11 +2,12 @@ export default function results(results) {
   return (
     <div>
       <h2>Results</h2>
-      <ul>
-        {results.map(result => {
-          return <li><a href={result.url}>{result.title}</a></li>
-        })}
-      </ul>
-    </div>
+      {results.map(resultSection => {
+        return <div><h3>{resultSection.section}</h3> <ul >{results.map(result => {
+          if (resultSection.section === result.section)
+            return <div><li><a href={result.url}>{result.title})</a></li></div>
+        })}</ul></div>
+      })}
+    </div >
   )
 }
