a39347c9d9cfeb4d58b91bfa2c101a79474c24c9
mattvioli <mattvioli@gmail.com>
2021-08-11 14:46:32 +1000

    use new Set to create unique array

--- components/ui/results.js
+++ components/ui/results.js
@@ -2,7 +2,7 @@ export default function results(results) {
   return (
     <div>
       <h2>Results</h2>
-      {results.map(resultSection => {
+      {Array.from(new Set(results)).map(resultSection => {
         return <div><h3>{resultSection.section}</h3> <ul >{results.map(result => {
           if (resultSection.section === result.section)
             return <div><li><a href={result.url}>{result.title})</a></li></div>
